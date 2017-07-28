93c0f1e4b560b70561f3bd780d71d99a27e343a4
freddinalcantara <freddyn.alcantara@gmail.com>
2017-07-27 23:34:14 -0400

    Adding Editing Client

--- SARC.WForm/Forms/FormCliente.cs
+++ SARC.WForm/Forms/FormCliente.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -30,16 +31,53 @@ namespace SARC.WForm
 
         private void metroButton3_Click(object sender, EventArgs e)
         {
-            //TO-DO: agregar validaciones
-            var Cliente = new Cliente {
+            var Cliente = new Cliente
+            {
                 Id = txtIdentificacion.Text,
                 Name = txtNombres.Text,
                 LastName = txtApellidos.Text,
                 PhoneNumber = txtNumeroTelf.Text,
                 Celphone = txtCelular.Text
             };
-            _dbContext.Clients.Add(Cliente);
+            RegistrarPersona(Cliente);
+        }
+
+        public bool EditarPersona(Cliente cliente)
+        {
+            _dbContext.Clients.Attach(cliente);
+            _dbContext.Entry(cliente).State = EntityState.Modified;
+
             _dbContext.SaveChanges();
+
+            return true;
+        }
+
+        public bool RegistrarPersona(Cliente cliente)
+        {
+
+            _dbContext.Clients.Add(cliente);
+            _dbContext.SaveChanges();
+
+            return true;
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            var ClientId = metroGrid1.SelectedCells[0].Value.ToString();
+            var Client = _dbContext.Clients.FirstOrDefault(c => c.Id == ClientId);
+            _dbContext.Clients.Remove(Client);
+            _dbContext.SaveChanges();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            var ClientId = metroGrid1.SelectedCells[0].Value.ToString();
+            var Client = _dbContext.Clients.FirstOrDefault(c => c.Id == ClientId);
+            txtIdentificacion.Text = Client.Id;
+            txtNombres.Text = Client.Name;
+            txtApellidos.Text = Client.LastName;
+            txtNumeroTelf.Text = Client.PhoneNumber;
+            txtCelular.Text = Client.Celphone;
         }
     }
 }
